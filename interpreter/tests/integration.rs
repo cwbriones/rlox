106a2137ace81ea22362dcd86f72eedbd01a8d4c
Christian Briones <cwbriones@gmail.com>
2018-03-13 15:55:33 -0700

    incorporate lang tests from crafting-interpreters repo
    These are not passing at the moment, but it is now possible to run
    against all of these examples.

--- interpreter/tests/integration.rs
+++ interpreter/tests/integration.rs
@@ -13,13 +13,22 @@ const BINARY: &str = "../target/release/rlox";
 
 const EXPECT: &str = "expect: ";
 const EXPECT_ERR: &str = "expect runtime error: ";
+const EXPECT_PARSE_ERR: &str = "Error at";
+
 const ERR_LOG: &str = "[error]: ";
 
-fn execute_test(filename: &str) {
+fn line_filter(mut line: String) -> Option<String> {
+    line.find("// expect")
+        .or_else(|| line.find("// Error at"))
+        .map(|idx| line.split_off(idx + 3)) // remove the comment
+}
+
+fn execute_test(mod_path: &str, filename: &str) {
     fs::metadata(BINARY).expect("Could not locate binary");
 
     let mut path = env::current_dir().unwrap();
     path.pop();
+    path.push(mod_path);
     path.push(filename);
     println!("{:?}", path);
 
@@ -31,9 +40,7 @@ fn execute_test(filename: &str) {
     let expects =
         file.lines()
             .map(|l| l.expect("should be valid UTF-8"))
-            .filter_map(|mut line| {
-                line.find("// expect").map(|idx| line.split_off(idx + 3))
-            });
+            .filter_map(line_filter);
 
     let mut expected_out = String::new();
     let mut expected_err = String::new();
@@ -46,6 +53,14 @@ fn execute_test(filename: &str) {
             let o = &expect[EXPECT_ERR.len()..].trim();
             expected_err.push_str(ERR_LOG);
             expected_err.push_str(o);
+            expected_err.push('\n');
+        } else if expect.starts_with(EXPECT_PARSE_ERR) {
+            let i = expect.find(": ").unwrap();
+            // FIXME: This transforms the expected parse error into the format rlox reports,
+            // consider making this the same as clox.
+            expected_err.push_str(ERR_LOG);
+            expected_err.push_str("Parse: ");
+            expected_err.push_str(&expect[i + 2..]);
             expected_err.push('\n');
         }
     }
@@ -68,31 +83,335 @@ fn execute_test(filename: &str) {
     };
 }
 
-macro_rules! define_test (
-    ($name:ident) => (
-        #[test]
-        fn $name() {
-            let filename = concat!("tests/", stringify!($name), ".lox");
-            execute_test(filename);
+macro_rules! define_test_mod (
+    ($mod:ident, $($testcase:ident),+) => (
+        mod $mod {
+            use super::execute_test;
+
+            const PATH: &str = concat!("lox-tests/", stringify!($mod));
+            define_tests!($($testcase),+);
         }
     );
 );
 
-define_test!(arithmetic);
-define_test!(variables_and_scope);
-define_test!(if_statement);
-define_test!(while_loop);
-define_test!(for_loop);
-define_test!(fibonacci);
-define_test!(logical_operators);
-define_test!(loop_break);
-define_test!(functions);
-define_test!(resolving);
-define_test!(equality);
-define_test!(native);
-define_test!(class);
-define_test!(inheritance);
-define_test!(lambda);
-define_test!(list);
+macro_rules! define_tests (
+    ($testcase:ident) => (
+        #[test]
+        fn $testcase() {
+            let filename = concat!(stringify!($testcase), ".lox");
+            execute_test(PATH, filename);
+        }
+    );
+    ($testcase:ident, $($rest:ident),*) => (
+        #[test]
+        fn $testcase() {
+            let filename = concat!(stringify!($testcase), ".lox");
+            execute_test(PATH, filename);
+        }
+        define_tests!($($rest),*);
+    );
+);
 
-define_test!(divide_by_zero);
+define_test_mod!(assignment,
+    associativity,
+    global,
+    grouping,
+    infix_operator,
+    local,
+    prefix_operator,
+    syntax,
+    to_this,
+    undefined
+);
+
+define_test_mod!(block, empty, scope);
+
+define_test_mod!(bool, equality, not);
+
+define_test_mod!(call, bool, nil, num, object, string);
+
+define_test_mod!(class,
+    empty, inherited_method, local_reference_self, reference_self);
+
+define_test_mod!(closure,
+    assign_to_closure,
+    assign_to_shadowed_later,
+    close_over_function_parameter,
+    close_over_later_variable,
+    close_over_method_parameter,
+    closed_closure_in_function,
+    nested_closure,
+    open_closure_in_function,
+    reference_closure_multiple_times,
+    reuse_closure_slot,
+    shadow_closure_with_local,
+    unused_closure,
+    unused_later_closure);
+
+define_test_mod!(comments,
+    line_at_eof,
+    only_line_comment,
+    only_line_comment_and_line,
+    unicode);
+
+define_test_mod!(constructor,
+    arguments,
+    call_init_explicitly,
+    default,
+    default_arguments,
+    early_return,
+    extra_arguments,
+    init_not_method,
+    missing_arguments,
+    return_in_nested_function,
+    return_value);
+
+define_test_mod!(expressions,
+    evaluate,
+    parse);
+
+define_test_mod!(field,
+    call_function_field,
+    call_nonfunction_field,
+    get_and_set_method,
+    get_on_bool,
+    get_on_class,
+    get_on_function,
+    get_on_nil,
+    get_on_num,
+    get_on_string,
+    many,
+    method,
+    method_binds_this,
+    on_instance,
+    set_evaluation_order,
+    set_on_bool,
+    set_on_class,
+    set_on_function,
+    set_on_nil,
+    set_on_num,
+    set_on_string,
+    undefined);
+
+define_test_mod!(for_statement,
+    class_in_body,
+    closure_in_body,
+    fun_in_body,
+    return_closure,
+    return_inside,
+    scope,
+    statement_condition,
+    statement_increment,
+    statement_initializer,
+    syntax,
+    var_in_body);
+
+define_test_mod!(function,
+    body_must_be_block,
+    empty_body,
+    extra_arguments,
+    local_mutual_recursion,
+    local_recursion,
+    missing_arguments,
+    missing_comma_in_parameters,
+    mutual_recursion,
+    parameters,
+    print,
+    recursion,
+    too_many_arguments,
+    too_many_parameters);
+
+define_test_mod!(if_statement,
+    class_in_else,
+    class_in_then,
+    dangling_else,
+    else_clause,
+    fun_in_else,
+    fun_in_then,
+    if_statement,
+    truth,
+    var_in_else,
+    var_in_then);
+
+define_test_mod!(inheritance,
+    inherit_from_function,
+    inherit_from_nil,
+    inherit_from_number,
+    inherit_methods,
+    parenthesized_superclass,
+    set_fields_from_base_class);
+
+// define_test_mod!(limit,
+//     loop_too_large,
+//     reuse_constants,
+//     stack_overflow,
+//     too_many_constants,
+//     too_many_locals,
+//     too_many_upvalues);
+
+define_test_mod!(logical_operator,
+    and,
+    and_truth,
+    or,
+    or_truth);
+
+define_test_mod!(method,
+    arity,
+    empty_block,
+    extra_arguments,
+    missing_arguments,
+    not_found,
+    refer_to_name,
+    too_many_arguments,
+    too_many_parameters);
+
+define_test_mod!(nil, literal);
+
+define_test_mod!(number,
+    decimal_point_at_eof,
+    leading_dot,
+    literals,
+    trailing_dot);
+
+define_test_mod!(operator,
+    add,
+    add_bool_nil,
+    add_bool_num,
+    add_bool_string,
+    add_nil_nil,
+    add_num_nil,
+    add_string_nil,
+    comparison,
+    divide,
+    divide_nonnum_num,
+    divide_num_nonnum,
+    equals,
+    equals_class,
+    equals_method,
+    greater_nonnum_num,
+    greater_num_nonnum,
+    greater_or_equal_nonnum_num,
+    greater_or_equal_num_nonnum,
+    less_nonnum_num,
+    less_num_nonnum,
+    less_or_equal_nonnum_num,
+    less_or_equal_num_nonnum,
+    multiply,
+    multiply_nonnum_num,
+    multiply_num_nonnum,
+    negate,
+    negate_nonnum,
+    not,
+    not_class,
+    not_equals,
+    subtract,
+    subtract_nonnum_num,
+    subtract_num_nonnum);
+
+define_test_mod!(print, missing_argument);
+
+define_test_mod!(regression, regression);
+
+define_test_mod!(return_statement,
+    after_else,
+    after_if,
+    after_while,
+    at_top_level,
+    in_function,
+    in_method,
+    return_nil_if_no_value);
+
+define_test_mod!(scanning,
+    identifiers,
+    keywords,
+    numbers,
+    punctuators,
+    strings,
+    whitespace);
+
+define_test_mod!(string,
+    error_after_multiline,
+    literals,
+    multiline,
+    unterminated);
+
+define_test_mod!(super_keyword,
+    bound_method,
+    call_other_method,
+    call_same_method,
+    closure,
+    constructor,
+    extra_arguments,
+    indirectly_inherited,
+    missing_arguments,
+    no_superclass_bind,
+    no_superclass_call,
+    no_superclass_method,
+    parenthesized,
+    reassign_superclass,
+    super_at_top_level,
+    super_in_closure_in_inherited_method,
+    super_in_inherited_method,
+    super_in_top_level_function,
+    super_without_dot,
+    super_without_name,
+    this_in_superclass_method);
+
+define_test_mod!(this,
+    closure,
+    nested_class,
+    nested_closure,
+    this_at_top_level,
+    this_in_method,
+    this_in_top_level_function);
+
+define_test_mod!(variable,
+    collide_with_parameter,
+    duplicate_local,
+    duplicate_parameter,
+    early_bound,
+    in_middle_of_block,
+    in_nested_block,
+    local_from_method,
+    redeclare_global,
+    redefine_global,
+    scope_reuse_in_different_blocks,
+    shadow_and_local,
+    shadow_global,
+    shadow_local,
+    undefined_global,
+    undefined_local,
+    uninitialized,
+    unreached_undefined,
+    use_false_as_var,
+    use_global_in_initializer,
+    use_local_in_initializer,
+    use_nil_as_var,
+    use_this_as_var);
+
+define_test_mod!(while_statement,
+    class_in_body,
+    closure_in_body,
+    fun_in_body,
+    return_closure,
+    return_inside,
+    syntax,
+    var_in_body);
+
+// define_test!(variables_and_scope);
+// define_test!(if_statement);
+// define_test!(while_loop);
+// define_test!(for_loop);
+// define_test!(fibonacci);
+// define_test!(logical_operators);
+// define_test!(loop_break);
+// define_test!(functions);
+// define_test!(resolving);
+// define_test!(equality);
+// define_test!(native);
+// define_test!(class);
+// define_test!(inheritance);
+// define_test!(lambda);
+// define_test!(list);
+//
+// define_test!(divide_by_zero);
---
+++ lox-tests/README.md
@@ -0,0 +1,8 @@
+Tests
+=====
+
+These test cases are from the official crafting-interpreters repository.
+
+See [here](https://github.com/munificent/craftinginterpreters/tree/master/test).
+
+These files are only used in the tree-walking [interpreter tests](../interpreter/tests/integration.rs) at the moment.
